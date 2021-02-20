a28d9ea01a325c2a1452d92179e6b725029a5d09
Balazs Juhasz <juhaszbal@gmail.com>
2021-02-20 16:19:24 +0100

    mongodb exercise

---
+++ mongodb.js
@@ -0,0 +1,77 @@
+// CRUD Create read update delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Balazs',
+    //     age: 38,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Bela',
+    //       age: 53,
+    //     },
+    //     {
+    //       name: 'Mate',
+    //       age: 31,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert documents');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Shopping',
+          completed: false,
+        },
+        {
+          description: 'Housework',
+          completed: true,
+        },
+        {
+          description: 'Play piano',
+          completed: true,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert tasks');
+        }
+
+        console.log(result.ops);
+      }
+    );
+  }
+);
