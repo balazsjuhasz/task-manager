575430e57515999464c1f4b52c95d996b8d66f16
Balazs Juhasz <juhaszbal@gmail.com>
2021-02-24 19:15:51 +0100

    convert promises to async await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,12 +2,20 @@ require('../src/db/mongoose');
 
 const User = require('../src/models/user');
 
-// 60327f8979db0c27c5785bbd
+// User.findByIdAndUpdate('6033bb93f6033d2facb470e5', { age: 1 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => console.log(result))
+//   .catch((e) => console.log(e));
 
-User.findByIdAndUpdate('6033bb93f6033d2facb470e5', { age: 1 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then((result) => console.log(result))
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('6033bb93f6033d2facb470e5', 2)
+  .then((count) => console.log(count))
   .catch((e) => console.log(e));
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require('./db/mongoose');
 
 const User = require('./models/user');
 const Task = require('./models/task');
+const { translateAliases } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,63 +14,77 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Handle users
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => res.status(201).send(user))
-    .catch((e) => res.status(400).send(e));
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch(() => res.status(500).send());
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
+  try {
+    const user = await User.findById(_id);
 
-      res.send(user);
-    })
-    .catch(() => res.status(500).send());
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // Handle Tasks
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => res.status(201).send(task))
-    .catch((e) => res.status(400).send(e));
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then((tasks) => res.send(tasks))
-    .catch(() => res.send.status(500).send());
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        res.status(404).send();
-      }
+  try {
+    const task = await Task.findById(_id);
 
-      res.send(task);
-    })
-    .catch(() => res.status(500).send());
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // Listen on port
