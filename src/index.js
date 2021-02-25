a65cff1d3a2b0dee414a98ca07e7bab60ada6b69
Balazs Juhasz <juhaszbal@gmail.com>
2021-02-25 14:32:21 +0100

    remove dummy line

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ require('./db/mongoose');
 
 const User = require('./models/user');
 const Task = require('./models/task');
-const { findByIdAndDelete } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
