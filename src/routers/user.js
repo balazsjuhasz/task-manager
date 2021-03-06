95bd7f070f36c9ab7fabcc682fe8c57099d8805b
Balazs Juhasz <juhaszbal@gmail.com>
2021-03-06 18:58:50 +0100

    finisg user profile interface, store in database as PNG

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = new express.Router();
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -105,7 +107,13 @@ router.post(
     if (!req.file) {
       return res.status(400).send({ error: 'Please provide an image' });
     }
-    req.user.avatar = req.file.buffer;
+
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -119,6 +127,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
   req.user.avatar = undefined;
   await req.user.save();
   res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
 });
 
 module.exports = router;
