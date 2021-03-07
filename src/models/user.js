f819bf037d8e3e4a454129d51fc9e8b02b394fe6
Balazs Juhasz <juhaszbal@gmail.com>
2021-03-07 15:24:54 +0100

    add email sending

--- src/models/user.js
+++ src/models/user.js
@@ -70,7 +70,7 @@ userSchema.virtual('tasks', {
 // Accesible on instances
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewsecret');
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
